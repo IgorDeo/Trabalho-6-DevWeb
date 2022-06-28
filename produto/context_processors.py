ef92e7c602fdc9c6f04ce3c495c5f06de18b0d9c
Igor Deo Alves <igor.deo@grana.capital>
2022-06-28 00:17:00 -0300

    create context_processor for total_value

--- produto/context_processors.py
+++ produto/context_processors.py
@@ -7,6 +7,8 @@ def atualiza_valor_total(request):
     
     produtos = Produto.objects.all()
 
+    valor_total = sum([produto.preco * produto.quantidade for produto in produtos])
+
     return {
-        'valor_total': zip(produtos, forms)
+        'valor_total': valor_total
     }
